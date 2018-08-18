d8902a1ecf9c734de264677b8ebb15916b1a4d63
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-08-18 08:23:08 -0500

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ let socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Nick',
-        text: 'Yup that works for me.'
-    });
 });
 
 socket.on('disconnect', function () {
