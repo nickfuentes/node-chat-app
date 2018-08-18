c12620dc354522acbc575f71a8b06cffb7c52f74
Nickolas Fuentes <nickolasfuentes@Mark-1.attlocal.net>
2018-08-18 09:12:20 -0500

    Create generateMessage utility

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+let expect = require('expect');
+
+let {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('Should generate the correct message object', () => {
+        let from = 'Jen';
+        let text = 'Some message';
+        let message = generateMessage(from, text);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({from, text});
+    });
+});
